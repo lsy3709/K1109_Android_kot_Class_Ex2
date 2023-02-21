77c803addc5498e8c506a7db610461fd0a00f44d
lsy3709 <lsy3709@naver.com>
2023-02-21 15:13:37 +0900

    230221 6교시 저장

--- mytest1/test3_2/src/main/java/com/example/test3_2/MyClass2.kt
+++ mytest1/test3_2/src/main/java/com/example/test3_2/MyClass2.kt
@@ -1,5 +1,7 @@
 package com.example.test3_2
 
+import com.example.test3_2.MyClass.Companion.data
+
 // 최고 상위 영역 : 선언과 동시에 초깃값 할당.
 //val name = "이상용"
 val name2:String = "이상용2"
@@ -65,8 +67,163 @@ class Sub: Super {
     constructor(name: String): super(name)
 }
 
+open class Super2(name: String) {
+    var superData = 10
+    fun superFun() {
+        println("super class")
+    }
+}
 
+class Sub2(name: String) : Super2(name)
+
+open class Super3(name: String) {
+    open var superData = 10
+    open fun superFun() {
+        println("super class")
+    }
+}
+
+class Sub3(name: String) : Super3(name) {
+    override var superData = 20
+    override fun superFun(){
+        println("재정의 테스트 ")
+    }
+}
+
+open class Super4 {
+    var publicData = 10
+    protected var protectedData = 10
+    private var privateData = 10
+
+}
+
+class Sub4: Super4() {
+    fun subFun() {
+        publicData++
+        protectedData++
+        //privateData++
+    }
+}
+
+class NonDataClass(val name :String, val age: Int)
+
+data class DataClass(val name :String, val age: Int)
+
+// 타입을 명시 안하면 외부에서 사용 못함.
+val obj = object {
+    var data = 10
+    fun some() {
+        println("익명 클래스 테스트 ")
+    }
+}
+
+open class Super5 {
+    open var publicData = 10
+    open fun some() {
+        println("익명 클래스 사용 테스트 Super5 ")
+    }
+}
+
+// 익명 클래스 : 이름없음. 키워드를 object : 상속할 클래스, 인터페이스 표기 {}
+val obj2 = object:Super5() {
+    override var publicData = 20
+    override fun some() {
+        println("익명 클래스 사용 테스트  obj2")
+    }
+}
+
+class MyClass {
+    // 인스턴스 멤버
+    var outData = 20
+    // static 과 비슷한 효과,.클래스 멤버
+    companion object {
+        var data = 10
+        fun some() {
+            println("컴패니언 object 테스트 ")
+        }
+    }
+}
+
+//람다 함수 간단 예제, (익명함수)
+val sum = {no1:Int, no2:Int -> no1 + no2}
+
+// 매개변수 1개인 경우, it 사용하기 전.
+val sum2 = {no1:Int ->
+    println( no1)
+    30
+}
+
+// 매개변수 1개인 경우, it 사용하기
+// 함수 타입 : (Int)-> Unit
+val sum3: (Int)-> Unit = {println(it)}
+
+//매개변수 자리에 (함수가 들어감) => arg: (Int) -> Boolean
+fun FunTest(arg: (Int) -> Boolean):()->String {
+    val result = if(arg(10)){
+        "valid"
+    } else {
+        "invalid"
+    }
+    return{"고차함수 테스트 : $result"}
+}
 fun main() {
+
+    // ? 연산자를 이용해서 null 할당 가능
+    var data33: String? = null
+    // ? 연산자를 사용한 변수에 특정 함수에 접근시 ?. 기호를 사용하거나
+    // 또는 !! 예외 발생 연산자를 사용해야함.
+    // 참고. 널체크 가 빠지게 되면 컴파일러가 친절히 알려줌.
+    data33?.length
+
+    // 만약에, 널이 아니면 해당 길이를 반환하고,
+    // 만약에 널이면 지정한 값을 사용한다.
+    // nvl 함수와 비슷한 구조.
+    // 삼항 다항식 비슷한 구조.
+    data33?.length ?: 0
+
+    println("널 안정설 체크해보기. ")
+    println( data33!!.length ?: 0)
+
+    val result5 = FunTest({no -> no > 0 })
+    println(result5())
+
+   val resut3 = sum(10,20)
+    val result4 = sum2(10)
+    println("result4 의 값 확인 해보기 : $result4")
+
+
+    var result2 = MyClass.data
+    println(result2)
+    MyClass.some()
+    val myClass = MyClass()
+    myClass.outData
+
+
+    println(obj2.publicData)
+    obj2.some()
+    // 타입을 명시 안하면 외부에서 사용 못함.
+    println(obj.toString())
+
+    val nonData1 = NonDataClass("lsy2", 30)
+    val nonData2 = NonDataClass("lsy2", 30)
+    val dataClass1 = DataClass("lsy3",40)
+    val dataClass2 = DataClass("lsy3",40)
+    println(nonData1.equals(nonData2))
+    println(dataClass1.equals(dataClass2))
+    println(nonData1.toString())
+    println(dataClass2.toString())
+
+    var obj4 = Sub4()
+    obj4.publicData
+
+
+    var obj3 = Sub3("lsy7")
+    println( obj3.superData)
+    obj3.superFun()
+
+    var obj = Sub2("lsy7")
+    println( obj.superData)
+    obj.superFun()
 
     val user5 = User3("lsy3",30)
     user5.someFun()
