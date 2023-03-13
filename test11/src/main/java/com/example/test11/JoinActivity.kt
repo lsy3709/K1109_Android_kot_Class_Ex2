a7dce75793890eeaa323a642e1c14c2a0e067d03
lsy3709 <lsy3709@naver.com>
2023-03-13 16:43:32 +0900

    230313 7교시 저장.

---
+++ test11/src/main/java/com/example/test11/JoinActivity.kt
@@ -0,0 +1,11 @@
+package com.example.test11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
