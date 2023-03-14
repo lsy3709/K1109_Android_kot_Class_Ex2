5b37bbe5a4c3fa9928d573e538814a443dbe2fab
lsy3709 <lsy3709@naver.com>
2023-03-14 09:48:13 +0900

    230314 1교시 저장.

---
+++ test11/src/main/java/com/example/test11/TestFragment.kt
@@ -0,0 +1,34 @@
+package com.example.test11
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.test11.databinding.FragmentOneBinding
+import com.example.test11.databinding.FragmentTestBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TestFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TestFragment : Fragment() {
+    lateinit var binding: FragmentTestBinding
+
+     override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+         binding = FragmentTestBinding.inflate(inflater, container, false)
+         return binding.root
+    }
+
+
+}
