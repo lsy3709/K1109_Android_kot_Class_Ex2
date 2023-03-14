9d037c5ec2507bee4f628fc4b166263e1ea7341f
lsy3709 <lsy3709@naver.com>
2023-03-14 11:23:24 +0900

    230314 3교시 저장.1

--- test11/src/main/java/com/example/test11/MainActivity342.kt
+++ test11/src/main/java/com/example/test11/MainActivity342.kt
@@ -15,43 +15,54 @@ import com.example.test11.databinding.Item342Binding
 class MainActivity342 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 리사이클러뷰가 보여주게 될 레이아웃.
         val binding = ActivityMain342Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 임의로 보여줄 문자열 배열.
         val datas = mutableListOf<String>()
         for(i in 1..9){
             datas.add("Item $i")
         }
+
+        // 만들었던 뷰홀더, 어댑터를 연결 시켜주는 부분.
+        // 리사이클러뷰 출력 하는 부분
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = MyAdapter(datas)
+        // 부가적인 옵션, 뒤에서 이미지 넣는 예제 확인 해 볼 예정.
         binding.recyclerView.addItemDecoration(
             DividerItemDecoration(this,
             LinearLayoutManager.VERTICAL)
         )
     }
-}
 
-class MyViewHolder(val binding: Item342Binding): RecyclerView.ViewHolder(binding.root)
 
-class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    class MyViewHolder(val binding: Item342Binding): RecyclerView.ViewHolder(binding.root)
 
-    override fun getItemCount(): Int{
-        Log.d("kkang", "init datas size: ${datas.size}")
-        return datas.size
-    }
+    class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder
-            = MyViewHolder(Item342Binding.inflate(LayoutInflater.from(parent.context), parent, false))
+        override fun getItemCount(): Int{
+            Log.d("kkang", "init datas size: ${datas.size}")
+            return datas.size
+        }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        Log.d("kkang","onBindViewHolder : $position")
-        val binding=(holder as MyViewHolder).binding
-        //뷰에 데이터 출력
-        binding.itemData.text= datas[position]
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder
+                = MyViewHolder(Item342Binding.inflate(LayoutInflater.from(parent.context), parent, false))
 
-        //뷰에 이벤트 추가
-        binding.itemRoot.setOnClickListener{
-            Log.d("kkang", "item root click : $position")
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            Log.d("kkang","onBindViewHolder : $position")
+            val binding=(holder as MyViewHolder).binding
+            //뷰에 데이터 출력
+            binding.itemData.text= datas[position]
+
+            //뷰에 이벤트 추가
+            binding.itemRoot.setOnClickListener{
+                Log.d("kkang", "item root click : $position")
+            }
         }
     }
+
+
 }
+
