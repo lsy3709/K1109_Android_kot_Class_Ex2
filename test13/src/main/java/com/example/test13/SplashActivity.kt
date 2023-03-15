68b4da7900995c4e81a106428231a5aaf5ae377a
lsy3709 <lsy3709@naver.com>
2023-03-15 11:46:25 +0900

    230315 3교시 저장1 실습 풀이 포함.

---
+++ test13/src/main/java/com/example/test13/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.test13
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+             },3000)
+
+
+    }
+}
