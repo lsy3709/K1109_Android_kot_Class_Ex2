7b6ebd7cc3cbf5f766ad68d827b0fe7f58e3b5df
lsy3709 <lsy3709@naver.com>
2023-03-16 17:46:59 +0900

    230316 7교시 저장1 갤러리 사진 선택 후 파일 저장 및 불러오기.실습 풀이, 일단 파일에 저장만 확인. 불러오기 내일..작업중

--- ch16_provider_test/src/main/java/com/example/ch16_provider_test/MainActivity2_backup.kt
+++ ch16_provider_test/src/main/java/com/example/ch16_provider_test/MainActivity2_backup.kt
@@ -11,7 +11,6 @@ import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
 import com.example.ch16_provider_test.databinding.ActivityMainBinding
-import java.io.ByteArrayInputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -19,11 +18,10 @@ import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2_backup : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var filePath: String
     lateinit var file: String
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +35,6 @@ class MainActivity : AppCompatActivity() {
         val requestGalleryLauncher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult())
         {
-            var inputStream = contentResolver.openInputStream(it.data!!.data!!)
             try {
 // calculateInSampleSize : 사진의 크기를 적절히 화면 비율에 맞게 재조정하는 함수
                 val calRatio = calculateInSampleSize(
@@ -49,13 +46,60 @@ class MainActivity : AppCompatActivity() {
                 option.inSampleSize = calRatio
 
                 // 넘어온 사진의 바이트로 읽은 객체 존재.
-//                var inputStream = contentResolver.openInputStream(it.data!!.data!!)
+                var inputStream = contentResolver.openInputStream(it.data!!.data!!)
 
+                val bitmap = BitmapFactory.decodeStream(inputStream, null, option)
 
-
+                bitmap?.let {
+                    binding.userImageView.setImageBitmap(bitmap)
+                } ?: let{
+                    Log.d("kkang", "bitmap null")
+                }
                 // 파일에 출력 하기.
 
+                val timeStamp: String =
+                    SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+                val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                val file = File.createTempFile(
+                    "LSYTEST_${timeStamp}_",
+                    ".jpg",
+                    storageDir
+                )
+                filePath = file.absolutePath
+                val photoURI: Uri = FileProvider.getUriForFile(
+                    this,
+                    "com.example.ch16_provider_test.fileprovider",
+                    file
+                )
 
+                // 갤러리에서 선택 된 사진를 it 으로 받았고,
+                // 위에서 사진의 정보를 바이트로 읽은 inputStream 존재.
+                // 내가 만든 임의의 파일에 쓰기 작업을 하는 코드.
+                try {
+                    val buff = ByteArray(1024 * 4)
+                    val os: OutputStream = FileOutputStream(file)
+                    while (true) {
+                        val readed: Int
+                        readed = inputStream!!.read(buff);
+
+                        if (readed == -1) {
+                            break;
+                        }
+                        os.write(buff, 0, readed);
+                        //write buff
+//                    downloaded += readed;
+                    }
+
+                    os.flush();
+                    os.close();
+
+                } catch (e: IOException) {
+                    e.printStackTrace();
+                } finally {
+                    if (inputStream != null) {
+                        inputStream.close();
+                    }
+                }
 
 
             }catch (e: Exception){
