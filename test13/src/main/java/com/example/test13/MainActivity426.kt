ba6aa6ba3c8cda215c530bfe4289f51fbafc7151
lsy3709 <lsy3709@naver.com>
2023-03-16 09:50:47 +0900

    230316 1교시 저장1 가상키보드, 화면고정, 스타일 적용법, 전체화면등

--- test13/src/main/java/com/example/test13/MainActivity426.kt
+++ test13/src/main/java/com/example/test13/MainActivity426.kt
@@ -1,7 +1,11 @@
 package com.example.test13
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import com.example.test13.databinding.ActivityMain426Binding
 
@@ -21,5 +25,27 @@ class MainActivity426 : AppCompatActivity() {
             manager.hideSoftInputFromWindow(currentFocus?.windowToken,
                 InputMethodManager.HIDE_NOT_ALWAYS)
         }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            // 화면의 기본 장식요소 (툴바, 액션등)이 시스템 창의 경계 안으로 들어가지 않도록 설정.
+            //window.insetsController
+            val controller = window.insetsController
+            // 현재 화면에 시스템 창 장식 정보를 가져온다.
+            if (controller != null) {
+                controller.hide(
+                    //상태바 , 네비게이션바 숨기는 기능.
+                    WindowInsets.Type.statusBars() or
+                            WindowInsets.Type.navigationBars())
+                // 탭바 숨겨진 후 잠시 나타나도록 하는 기능.
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
+
     }
 }
